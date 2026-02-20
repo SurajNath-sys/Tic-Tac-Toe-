14527d79ec989ca84d64104dd8adb9d5e4a60ed1
Suraj Nath <262309023+SurajNath-sys@users.noreply.github.com>
2026-02-20 16:01:12 +0530

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -4,7 +4,6 @@ public class tictactoe {
 
     static char[] board = {'0','1','2','3','4','5','6','7','8','9'};
 
-    // Function to display board
     static void showBoard() {
         System.out.println("\n        TIC TAC TOE        \n");
 
@@ -16,31 +15,26 @@ public class tictactoe {
         System.out.println();
     }
 
-    // Function to check win/draw
     static int checkWin() {
 
-        // Rows
         if (board[1] == board[2] && board[2] == board[3]) return 1;
         if (board[4] == board[5] && board[5] == board[6]) return 1;
         if (board[7] == board[8] && board[8] == board[9]) return 1;
 
-        // Columns
         if (board[1] == board[4] && board[4] == board[7]) return 1;
         if (board[2] == board[5] && board[5] == board[8]) return 1;
         if (board[3] == board[6] && board[6] == board[9]) return 1;
 
-        // Diagonals
         if (board[1] == board[5] && board[5] == board[9]) return 1;
         if (board[3] == board[5] && board[5] == board[7]) return 1;
 
-        // Check for empty cells
         for (int i = 1; i <= 9; i++) {
             if (board[i] == (char)('0' + i)) {
                 return -1;
             }
         }
 
-        return 0; // Draw
+        return 0; 
     }
 
     public static void main(String[] args) {
@@ -73,10 +67,11 @@ public class tictactoe {
         showBoard();
 
         if (status == 1)
-            System.out.println("🎉 Player " + (player - 1) + " wins!");
+            System.out.println(" Player " + (player - 1) + " wins!");
         else
-            System.out.println("🤝 Game Draw!");
+            System.out.println("Game Draw!");
 
         sc.close();
     }
+
 }
