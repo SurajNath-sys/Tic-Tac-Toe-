e8739fb5b95fd774ae6284481204bd747bf8b92b
Suraj Nath <262309023+SurajNath-sys@users.noreply.github.com>
2026-03-16 14:59:56 +0530

    Update tictactoe.java

--- tictactoe.java
+++ tictactoe.java
@@ -42,7 +42,7 @@ public class tictactoe
         do 
         {
             showBoard();
-            
+        
             player = (player % 2 != 0) ? 1 : 2;
             mark = (player == 1) ? 'X' : 'O';
             System.out.print("Player " + player + ", enter your choice: ");
